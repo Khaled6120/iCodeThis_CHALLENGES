6986202ba6f7f541632cff94591426e7612e0894
Khaled6120 <alnaes1234@gmail.com>
2023-02-28 17:49:48 +0300

    Challenge-16 Challenge-17 done!

---
+++ Chellenge-16/index.js
@@ -0,0 +1,85 @@
+const topics = document.querySelectorAll('.sidebar .topics .topic');
+const title = document.querySelector('.content .top-row h3');
+const paragraph = document.querySelector('.contents p');
+
+const notePad = [
+    {
+        title: "Intro to Journal",
+        contents: `Welcome to this journal, where we explore and reflect upon the many facets of our lives and the world around us. This journal serves as a platform for deep thinking, self-discovery, and inspiration. Whether you are a writer, artist, student, or simply someone who enjoys exploring new ideas, this journal has something for everyone.
+
+        As human beings, we are inherently curious creatures, always seeking to understand more about the world we live in and the experiences we have. It is through this innate curiosity that we are able to learn, grow, and make meaning of our lives. This journal is a space where we can explore and reflect upon the various ways in which we experience the world, both internally and externally.
+        
+        One of the unique features of this journal is its focus on interdisciplinary exploration. We believe that the most meaningful insights often come from looking at a particular topic from multiple perspectives. Therefore, we encourage contributions from a wide range of disciplines, including but not limited to psychology, philosophy, literature, art, and science.
+        
+        In this journal, we explore topics that are relevant to our everyday lives, such as relationships, identity, mental health, spirituality, and creativity. We also delve into larger societal issues, such as politics, social justice, and environmentalism. By addressing both the personal and the global, we hope to foster a deeper understanding of ourselves and the world we inhabit.
+        
+        In addition to written contributions, we also welcome visual art, poetry, and other forms of creative expression. We believe that creativity is a powerful tool for self-discovery and healing, and we encourage our contributors to explore their own creativity in whatever way feels most natural to them.
+        
+        Ultimately, this journal is a space for exploration, reflection, and growth. We hope that our readers find the articles and creative pieces contained within these pages to be thought-provoking and inspiring, and we look forward to continuing to build a community of curious, reflective individuals.
+        Welcome to this journal, where we explore and reflect upon the many facets of our lives and the world around us. This journal serves as a platform for deep thinking, self-discovery, and inspiration. Whether you are a writer, artist, student, or simply someone who enjoys exploring new ideas, this journal has something for everyone.
+
+        As human beings, we are inherently curious creatures, always seeking to understand more about the world we live in and the experiences we have. It is through this innate curiosity that we are able to learn, grow, and make meaning of our lives. This journal is a space where we can explore and reflect upon the various ways in which we experience the world, both internally and externally.
+        
+        One of the unique features of this journal is its focus on interdisciplinary exploration. We believe that the most meaningful insights often come from looking at a particular topic from multiple perspectives. Therefore, we encourage contributions from a wide range of disciplines, including but not limited to psychology, philosophy, literature, art, and science.
+        
+        In this journal, we explore topics that are relevant to our everyday lives, such as relationships, identity, mental health, spirituality, and creativity. We also delve into larger societal issues, such as politics, social justice, and environmentalism. By addressing both the personal and the global, we hope to foster a deeper understanding of ourselves and the world we inhabit.
+        
+        In addition to written contributions, we also welcome visual art, poetry, and other forms of creative expression. We believe that creativity is a powerful tool for self-discovery and healing, and we encourage our contributors to explore their own creativity in whatever way feels most natural to them.
+        
+        U`
+    },
+    {
+        title: "Dusk's Serenade",
+        contents: `As the sun began to set, the sky turned a brilliant shade of orange and pink, casting a warm glow over the landscape. The air was still, and the only sound was the distant chirping of crickets. A small group of children played tag in the nearby field, their laughter ringing out in the quiet of the evening. A family of rabbits hopped along the edge of the forest, stopping now and then to nibble on the grass. In the distance, a group of cows mooed softly, their heads bent as they grazed on the lush grass of the meadow.
+
+        As the light began to fade, a faint mist began to rise from the ground, casting a soft haze over everything. A group of fireflies emerged from the shadows, their tiny bodies glowing in the darkening twilight. A soft breeze picked up, rustling the leaves of the trees and bringing with it the scent of blooming flowers. A pair of barn owls hooted softly to each other, their calls echoing through the stillness.
+        
+        As the night fell, the stars began to twinkle in the clear sky, casting a gentle light over the landscape. The children had long since gone home, leaving behind only their echoes of laughter and joy. The rabbits had retreated to their burrows, and the cows had settled down to sleep. In the silence of the night, the world seemed almost magical, a place of wonder and mystery.
+        
+        As the hours ticked by, the world began to awaken once again. The sun began to rise, casting a warm glow over the landscape once more. The birds began to sing, their sweet melodies filling the air. The cows mooed, and the rabbits emerged from their burrows. The world was alive once more, filled with the promise of a new day.
+        
+        As the day progressed, the world continued to turn, and life went on. The children returned to the field, their laughter ringing out once more. The cows were herded into the barn for milking, and the rabbits continued to nibble on the grass. The world was full of activity, but even in the midst of all the noise and chaos, there was still a sense of peace and calm. The world was a beautiful place, full of wonder and mystery, and there was always something new to discover, something new to explore.`,
+    },
+    {
+        title: "Vibrant Rhythm of Urban Life",
+        contents: `The city streets were alive with energy, as people hurried back and forth, going about their daily business. Cars honked and screeched, jostling for position on the crowded roads. The sound of construction filled the air, as new buildings rose up out of the ground. The smell of food wafted through the streets, mingling with the scent of car exhaust and diesel fumes. Street vendors shouted out their wares, hawking everything from hot dogs to handmade crafts.
+
+        As the day wore on, the streets became even more crowded, and the noise grew even louder. The sun beat down mercilessly, making the air shimmer with heat. Sweat poured down the faces of the people as they rushed to their destinations, the sound of their footsteps echoing off the pavement.
+        
+        But even in the midst of all the chaos and noise, there was a sense of excitement and possibility. Anything could happen in the city, and every day brought with it the promise of something new and exciting. The world was a vast and unpredictable place, full of wonder and adventure, and the city was just one small part of it.
+        
+        As the day drew to a close, the streets began to empty, and the noise died down. The sound of car horns faded away, and the only sound was the soft hum of the city at rest. The streetlights flickered to life, casting a warm glow over the empty sidewalks. The city settled into a peaceful slumber, resting up for another day of excitement and adventure.`
+    },
+    {
+        title: "A Night in the Enchanted Forest",
+        contents: `The forest was alive with the sound of birdsong, as a gentle breeze rustled the leaves of the trees. A babbling brook wound its way through the underbrush, the sound of its flowing water a soothing background noise. The forest floor was covered in a thick layer of fallen leaves, crunching softly underfoot.
+
+        As the sun began to set, the trees cast long, dappled shadows across the forest floor. The birds settled down for the night, their songs fading away as the sky turned from blue to pink to orange. A family of deer emerged from the trees, their graceful forms silhouetted against the colorful sky. They moved slowly and cautiously, their ears twitching at the slightest sound.
+        
+        As the night fell, the forest came alive with the sound of nocturnal animals. Owls hooted softly in the trees, and the occasional bat swooped by overhead. The stars began to twinkle in the sky, casting a gentle light over the forest. Fireflies emerged from the underbrush, their tiny bodies glowing like miniature stars.
+        
+        As the night wore on, the forest became eerily quiet, save for the sound of the brook and the occasional rustling in the underbrush. The moon rose high in the sky, casting a pale glow over the trees. The world seemed almost magical in the moonlight, full of mystery and wonder.
+        
+        As the first light of dawn crept over the horizon, the forest began to awaken once again. The birds began to sing, their melodies gradually growing louder and more insistent. The deer retreated back into the trees, their elegant forms disappearing into the shadows. The world was alive once more, full of the promise of a new day.`
+    }
+];
+
+paragraph.textContent = notePad[0].contents;
+title.textContent = notePad[0].title;
+
+function checkCurrentChapter() {
+    for (let i = 0; i < topics.length; i++) {
+        topics[i].addEventListener('click', function () {
+            topics.forEach(t => t.classList.remove('active'));
+            topics[i].classList.add('active');
+            changeChapter(i);
+        });
+    }
+}
+
+checkCurrentChapter();
+
+function changeChapter(i) {
+    paragraph.textContent = notePad[i].contents;
+    title.textContent = notePad[i].title;
+}
